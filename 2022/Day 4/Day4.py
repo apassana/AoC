b08c706784638bd3384e117fbbc074fb7ace8c19
Alex Passanante <alexanderpassanante@gmail.com>
2024-10-16 03:05:17 -0700

    Update Day4.py

--- 2022/Day 4/Day4.py
+++ 2022/Day 4/Day4.py
@@ -17,16 +17,69 @@ def get_list() -> list[str]:
     return clean_list(a)
 
 
-def create_ranges(l: list[str]) -> list[list[int]]:
-    result = list()
-    for item in l:
+def create_range(l: str) -> list[list[int]]:
+    n = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+    result = []
+    r = []
+    a = ''
+    for i in l:
+        if i in n:
+            a += i
+        if i == '-':
+            if a != '':
+                r.append(int(a))
+            a = ''
+        if i == ',':
+            if a != '':
+                r.append(int(a))
+                result.append(r)
+            r = []
+            a = ''
+    r.append(int(a))
+    result.append(r)
+    return result
 
+
+def encompassed(l: list[list[int]]) -> bool:
+    a = l[0][0]
+    b = l[0][1]
+    c = l[1][0]
+    d = l[1][1]
+    if a <= c:
+        if d <= b:
+            return True
+    if c <= a:
+        if b <= d:
+            return True
+    return False
+
+
+def touching(l: list[list[int]]) -> bool:
+    a = l[0][0]
+    b = l[0][1]
+    c = l[1][0]
+    d = l[1][1]
+    if b < c:
+        return False
+    if d < a:
+        return False
+    return True
 
 
 def main():
     rans = get_list()
-    print(rans[0])
-
+    a = []
+    b = 0
+    c = 0
+    for i in rans:
+        a.append(create_range(i))
+    for i in a:
+        if encompassed(i):
+            b += 1
+        if touching(i):
+            c += 1
+    print("Part 1: ", b)
+    print("Part 2: ", c)
 
 
 if __name__ == '__main__':
